5cf6edddb586098a947132dc073508bd0f50199f
lokeshpusarla <lokeshpusarla100@gmail.com>
2024-08-29 20:35:12 +0530

    added routes

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 import express from 'express';
-// import bodyParser from 'body-parser';
-import staffRoutes from './routes/adminroutes.js';
 import sequelize from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import studentroutes from './routes/studentroutes.js';
+import {
+    studentroutes,
+    trainerroutes,
+    solutionroutes,
+    scoreroutes,
+    questionroutes,
+    questionCollegeRoute,
+    domainroute,
+    collegeroute,
+    batchroute,
+    adminroute
+  } from './routes';
 // import printRoutes from './conf/routes.js';
 
 dotenv.config();
@@ -14,9 +23,17 @@ const app = express();
 // app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
+app.use('/students', studentroutes);
+app.use('/trainers', trainerroutes);
+app.use('/solutions', solutionroutes);
+app.use('/scores', scoreroutes);
+app.use('/questions', questionroutes);
+app.use('/question-college', questionCollegeRoute);
+app.use('/domains', domainroute);
+app.use('/colleges', collegeroute);
+app.use('/batches', batchroute);
+app.use('/admins', adminroute);
 
-app.use('/api/staff', staffRoutes);
-app.use('/api',studentroutes);
 
 
 // if (process.env.NODE_ENV === 'development') {
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+import studentroutes from '../routes/studentroutes';
+import trainerroutes from '../routes/trianer_routes';
+import solutionroutes from '../routes/solutions_route';
+import scoreroutes from '../routes/score_route';
+import questionroutes from '../routes/questoinroutes';
+import questionCollegeRoute from '../routes/question_college_route';
+import domainroute from '../routes/domain_routes';
+import collegeroute from '../routes/college_routes';
+import batchroute from '../routes/batch_routes';
+import adminroute from '../routes/adminroutes';
+
+export {
+    studentroutes,trainerroutes,solutionroutes,scoreroutes,questionroutes,
+    questionCollegeRoute,domainroute,collegeroute,batchroute,adminroute
+}
